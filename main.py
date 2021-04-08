64dc134c6d118d07d5fa353061f27d738827da2d
pbraer <73088589+pbraer@users.noreply.github.com>
2021-04-08 17:25:19 +0700

    start

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hi')
+import codecs
+text = ''
+with codecs.open('input.txt', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            if text == '':
+                s = ''
+            else:
+                s = ' '
+            if line.find('\r') != -1:
+                text += s + line[:line.find('\r')]
+            elif line.find('\n') != -1:
+                text += s + line[:line.find('\n')]
+            else:
+                text += s + line
+words = text.split(' ')
+print(words)
