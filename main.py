4f87602ad491aa3d94e64c34258f9c76565b51df
darya752 <73088666+darya752@users.noreply.github.com>
2021-04-10 16:08:16 +0300

    dictionary_start

--- main.py
+++ main.py
@@ -14,12 +14,41 @@ with codecs.open('input.txt', 'r', encoding='utf-8') as f:
             else:
                 text += s + line
 words = text.split(' ')
-print(words)
 
-words_2 = []
+start_words = []
+middle_words = []
+finish_words = []
+
 for word in words:
-    if word not in words_2:
-        words_2.append(word)
-print(words_2)
-d = dict.fromkeys([words_2])
-print(d)
+    if word.istitle() == True:
+        start_words.append(word)
+    elif word[len(word) - 1] == '.':
+        finish_words.append(word)
+    else:
+        middle_words.append(word)
+
+start_words_var = []
+for word in start_words:
+    if word not in start_words_var:
+        start_words_var.append(word)
+
+dict_start_words = {}
+words_after = []
+position = -1
+for word in start_words_var:
+    for all_word in words:
+        position += 1
+        if word == all_word and position != len(words) - 1:
+            words_after.append(words[position + 1])
+    if word not in dict_start_words:
+        dict_start_words[word] = words_after
+    position = -1
+    words_after = []
+print(dict_start_words)
+
+finish_words_var = []
+
+
+dict_middle_words = {}
+dict_finish_words = {}
+
