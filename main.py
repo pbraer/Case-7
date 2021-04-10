dad756b83178270cc493d9bb65672336e3d80d79
darya752 <73088666+darya752@users.noreply.github.com>
2021-04-10 16:20:50 +0300

    dictionary_finish

--- main.py
+++ main.py
@@ -46,9 +46,42 @@ for word in start_words_var:
     words_after = []
 print(dict_start_words)
 
-finish_words_var = []
 
+middle_words_var = []
+for word in middle_words:
+    if word not in middle_words_var:
+        middle_words_var.append(word)
 
 dict_middle_words = {}
-dict_finish_words = {}
+words_after_m = []
+position_m = -1
+for word in middle_words_var:
+    for all_word in words:
+        position_m += 1
+        if word == all_word and position_m != len(words) - 1:
+            words_after_m.append(words[position_m + 1])
+    if word not in dict_middle_words:
+        dict_middle_words[word] = words_after_m
+    position_m = -1
+    words_after_m = []
+print(dict_middle_words)
 
+
+finish_words_var = []
+for word in finish_words:
+    if word not in finish_words_var:
+        finish_words_var.append(word)
+
+dict_finish_words = {}
+words_after_f = []
+position_f = -1
+for word in finish_words_var:
+    for all_word in words:
+        position_f += 1
+        if word == all_word and position_f != len(words) - 1:
+            words_after_f.append(words[position_f + 1])
+    if word not in dict_finish_words:
+        dict_finish_words[word] = words_after_f
+    position_f = -1
+    words_after_f = []
+print(dict_finish_words)
